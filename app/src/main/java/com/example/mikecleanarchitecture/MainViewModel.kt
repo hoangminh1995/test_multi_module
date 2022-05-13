44921c7cc476142519d625565e2facf4c52d061a
minhmike <>
2022-05-13 14:22:05 +0700

    handle base

--- app/src/main/java/com/example/mikecleanarchitecture/MainViewModel.kt
+++ app/src/main/java/com/example/mikecleanarchitecture/MainViewModel.kt
@@ -2,7 +2,10 @@ package com.example.mikecleanarchitecture
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,9 +13,20 @@ class MainViewModel @Inject constructor(
     private val weatherService: WeatherService
 ) : ViewModel() {
 
-    suspend fun getWeather() {
-        try {
+    fun getWeather() {
+        launchApi {
             weatherService.getWeather("ho chi minh")
+        }
+    }
+}
+
+fun ViewModel.launchApi(
+    coroutineScope: CoroutineScope = viewModelScope,
+    action: suspend () -> Unit
+) {
+    viewModelScope.launch {
+        try {
+            action.invoke()
         } catch (e: Exception) {
             Log.e("minh", "${e.message}")
         }
